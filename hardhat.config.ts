1807ff6493db02f060603a578c882ba2748e4b1e
Mark Howard <bark4mark@gmail.com>
2023-03-30 23:37:29 +0100

    adding config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,16 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: process.env.GOERLI_RPC,
+      accounts: [process.env.PRIVATE_KEY as string]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,24 +1,14 @@
 import { ethers } from "hardhat";
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = ethers.utils.parseEther("0.001");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+const main = async () => {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+  const PartialDelegation = await ethers.getContractFactory("PartialDelegation");
+  const partialDelegation = await PartialDelegation.deploy();
+  console.log("Contract address:", partialDelegation.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main();
+
+// 0xF8B53db7136F9Fbf2F7C0a79e4Ff5670f10061c3
+// npx hardhat verify --network goerli 0xF8B53db7136F9Fbf2F7C0a79e4Ff5670f10061c3
